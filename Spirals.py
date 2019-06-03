ce387eea3ff22c47d4ce1c1f3dc35421fd77c1be
Yohann Faure <faure.yohann@gmail.com>
2019-06-03 16:39:00 -0400

    added spirals

--- Spirals.py
+++ Spirals.py
@@ -12,47 +12,49 @@ import matplotlib.pyplot as plt
 from FunctionsModule import *
 from TiltFinder import *
 
+coords = [(0,0),(0,0)]
 
 
 # Simple mouse click function to store coordinates
 def onclick(event):
-    global ix, iy
     ix, iy = event.xdata, event.ydata
-    # print 'x = %d, y = %d'%(
-    #     ix, iy)
-    # assign global variable to access outside of function
-    global coords
     coords.append((ix, iy))
-    # Disconnect after 2 clicks
-    if len(coords) == -1:
-        fig.canvas.mpl_disconnect(cid)
-        plt.close(1)
+    coords.append((ix, iy))
     return(None)
 
+def onresize(event):
+    print(coords.pop())
+    return(None)
 
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser(description = 'arguments')
     parser.add_argument("location", help = "File to use as image data.",type = str)
-    parser.add_argument("--inc", help = "Inclination of the disk, add 360 if it's negative ('-' is considered as a system prefix). (deg)",type = float)
-    parser.add_argument("--pa", help = "Position angle of the disk, add 360 if it's negative ('-' is considered as a system prefix). (deg)",type = float)
+    parser.add_argument("--inc", help = "Inclination of the disk, add 360 if it's negative ('-' is considered as a system prefix). (deg)",type = float,default=None)
+    parser.add_argument("--pa", help = "Position angle of the disk, add 360 if it's negative ('-' is considered as a system prefix). (deg)",type = float,default=None)
     args = parser.parse_args()
     location=args.location
     header,image=openimage(location)
-    coords = []
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
     ax.imshow(image)
     # Call click func
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
+#    cid = fig.canvas.mpl_connect('resize_event', onresize)
+    cif = fig.canvas.mpl_connect('draw_event', onresize)
     plt.show(1)
-    center=(1500,1500)
-    aaa=TiltFinder(location,center=center,width=600)
-    inc,pa=aaa[2:4]
+    #####
+    print(coords)
+    center=(511.74577572964677, 512.5657729367406)
+    print(args.pa,args.inc)
+    if args.pa==None or args.inc==None:
+        aaa=TiltFinder(location,center=center,width=600)
+        inc,pa=aaa[2:4]
+    else :
+        inc,pa=args.inc,args.pa
     coords=np.array(coords)
     coords=coords-np.array(center)
     x,y,r,theta=deprojectedcoordinatespairs(np.transpose(coords),inc,pa)
     plt.scatter(theta,r)
     plt.scatter(theta+2*np.pi,r)
     plt.show()
-
