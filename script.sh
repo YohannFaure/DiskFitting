29c1e42f8e433710267306c22382d9b1f7f163fd
Yohann Faure <faure.yohann@gmail.com>
2019-06-07 12:31:45 -0300

    added script

---
+++ script.sh
@@ -0,0 +1,15 @@
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/BigOpti.npy' --suffix 'part23'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part23.npy' --suffix 'part24'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part24.npy' --suffix 'part25'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part25.npy' --suffix 'part26'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part26.npy' --suffix 'part27'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part27.npy' --suffix 'part28'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part28.npy' --suffix 'part29'
+
+python3 ModelingEmcee.py 300 1000 16 --resume 'results/optimization/opti_37_3000_1000part29.npy' --suffix 'part30'
